ce5ce7db8b96b4f973fd807cb84a30fac26b8d29
Leo <leowzukw@vmail.me>
2014-11-22 17:14:26 +0100

    Add warning when the number is out of range

--- src/exec_cmd.ml
+++ src/exec_cmd.ml
@@ -41,7 +41,11 @@ let num_cmd_to_cmd ~cmd_list number =
   (* Verify that the number is not out of the list *)
   if (List.length cmd_list) < number
   then
-    ""
+      begin
+          (* TODO Make this printing configurable *)
+          printf "Out of range. Do not do anything.";
+          ""
+      end
   else
     begin
       let cmd_to_exec = List.nth cmd_list number in
