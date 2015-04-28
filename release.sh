01a422668fb75ed66af77b44fb8ebe3daf955d15
Leo <leowzukw@vmail.me>
2015-04-28 18:40:53 +0200

    Add release script

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to realise easily new version
+
+# Set version
+./version_set.sh $1
+
+# Record changes
+git commit -a -m "Version $(cat ./VERSION)"
+git tag -s v$(cat VERSION) -m "Release $(cat VERSION)"
+
+# Cleanup dist directory to put the new archives
+rm -r dist/*
+./0install.sh
