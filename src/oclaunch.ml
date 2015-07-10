1723cb8a5b9f9ee0f9ce53676b670a0d6b761606
Leo <leowzukw@vmail.me>
2015-07-10 13:09:39 +0200

    Add timer to see how long it was

--- src/oclaunch.ml
+++ src/oclaunch.ml
@@ -46,8 +46,16 @@ let version_number = "0.2.2.1-dev";;
 let build_info = ( "Build with OCaml version " ^ (Sys.ocaml_version) ^ " on " ^ (Sys.os_type) );;
 
 let () =
+  (* Store begin time *)
+  let start = Time.(now () |> to_float) in
+
+  (* Running commands *)
   Command.run ~version:version_number ~build_info:build_info
   Command_def.commands;
+
+  (* Display total running time, to float is a number of secconds *)
+  Messages.debug Time.(now () |> to_float |> (-.) start |> ( *. ) (-1.)
+  |> sprintf "Runned during %f second(s)");
   (* Reset display *)
   Messages.reset ()
 ;;
