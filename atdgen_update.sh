d50bbc06f4deff786ae4b19be063f16b5e66e873
Leo <leowzukw@vmail.me>
2015-07-10 10:34:37 +0200

    Add script to update atdgen-generated files

---
+++ atdgen_update.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# Copyright © Joly Clément, 2014-2015
+#
+#  leowzukw@vmail.me
+#
+#  Ce logiciel est un programme informatique servant à exécuter
+#  automatiquement des programmes à l'ouverture du terminal.
+#
+#  Ce logiciel est régi par la licence CeCILL soumise au droit français et
+#  respectant les principes de diffusion des logiciels libres. Vous pouvez
+#  utiliser, modifier et/ou redistribuer ce programme sous les conditions
+#  de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA
+#  sur le site "http://www.cecill.info".
+#
+#  En contrepartie de l'accessibilité au code source et des droits de copie,
+#  de modification et de redistribution accordés par cette licence, il n'est
+#  offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+#  seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+#  titulaire des droits patrimoniaux et les concédants successifs.
+#
+#  A cet égard  l'attention de l'utilisateur est attirée sur les risques
+#  associés au chargement,  à l'utilisation,  à la modification et/ou au
+#  développement et à la reproduction du logiciel par l'utilisateur étant
+#  donné sa spécificité de logiciel libre, qui peut le rendre complexe à
+#  manipuler et qui le réserve donc à des développeurs et des professionnels
+#  avertis possédant  des  connaissances  informatiques approfondies.  Les
+#  utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+#  logiciel à leurs besoins dans des conditions permettant d'assurer la
+#  sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+#  à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+#
+#  Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
+#  pris connaissance de la licence CeCILL, et que vous en avez accepté les
+#  termes.
+
+atdgen -t ./src/settings.atd
+atdgen -j ./src/settings.atd
+atdgen -v ./src/settings.atd
+atdgen -b ./src/tmp_biniou.atd
+atdgen -t ./src/tmp_biniou.atd
+atdgen -v ./src/tmp_biniou.atd
+# Test, to be shure that atgen commands above run
+echo "Atdgen executed, files updated"
