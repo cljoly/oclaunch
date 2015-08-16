be2e88732320f1aa8504369ae3630f476666b1fe
Leo <leowzukw@vmail.me>
2015-08-16 12:39:40 +0200

    Add test for some functions of the module Edit_command

--- src/test/edit_t.ml
+++ src/test/edit_t.ml
@@ -36,9 +36,78 @@
 
 open Core.Std;;
 
-(* A module launching all tests *)
+(* A module containing tests for src/edit_command.ml *)
 
-let () =
-    Alcotest.run "Test suite for the project"
-        (List.concat [ Ec_t.alco ; Exec_t.alco ; Edit_t.alco ])
+(* Function epur ============================= *)
+let epur test solution () =
+  let actual = Edit_command.epur test in
+  OUnit.assert_equal actual solution
 ;;
+
+(* Data for above test *)
+let ll_data = [
+  ( [ "cmd1" ; "cmd2"; "" ], [ "cmd1" ; "cmd2" ], "Canonical case" );
+  ( [ "" ; "cmd1" ; "" ; "" ; "cmd2"; "" ], [ "cmd1" ; "cmd2" ], "Harder case" );
+  ( [], [], "Empty list" );
+  ( [ "" ; "" ; "" ], [], "Empty list resulting of empty strings" );
+]
+
+let llt_l =
+  List.map ll_data ~f:(fun (t, s, name) -> ( (epur t s), name))
+  |> List.map ~f:(fun ( f,name ) -> (name, `Quick, f))
+;;
+(* =========================================== *)
+
+
+(* Function new_list ========================= *)
+let n_list ( current_list, position, new_items ) solution () =
+  let actual = Edit_command.new_list current_list position new_items in
+  OUnit.assert_equal actual solution
+;;
+
+(* Data for above test *)
+let nl_data = [
+  ( ( [ "cmd1" ; "cmd2" ; "cmd3" ], 1, ["new_cmd"] ), [ "cmd1" ; "new_cmd" ; "cmd2" ; "cmd3" ], "Canonical case 1" );
+  ( ( [ "cmd1" ; "cmd2" ; "cmd3" ; "cmd4" ; "cmd5" ], 2, [ "cmd"  ; "cmd" ; "cmd" ] ),
+      [ "cmd1" ; "cmd2" ; "cmd"  ; "cmd" ; "cmd" ; "cmd3" ; "cmd4" ; "cmd5" ],
+      "Canonical case 2" );
+  ( ( [ "cmd1" ; "cmd2" ; "cmd3" ], 0, ["new_cmd"] ), [ "new_cmd" ; "cmd1" ;
+  "cmd2" ; "cmd3" ], "Insert on top" );
+  ( ( [ "cmd1" ; "cmd2" ; "cmd3" ; "cmd4" ; "cmd5" ], 4, ["new_cmd"] ), [ "cmd1"
+  ; "cmd2" ; "cmd3" ; "cmd4" ; "new_cmd" ; "cmd5" ], "Insert bottom" );
+  ( ( [], 0, [] ), [], "Empty list" );
+  ( ( [], 0, [ "new" ; "element" ] ), [ "new" ; "element" ], "Insertion of new elements in an empty list" );
+  ( ( [], 5, [ "new" ; "element" ] ), [ "new" ; "element" ], "Out of bound (inserting elements)" );
+  ( ( [], 5, [] ), [], "Out of bound (inserting nothing)" );
+]
+
+let big =
+  (
+    "Big list",
+    `Slow,
+    (fun () ->
+      let l = List.init 1_000_000 ~f:(fun a -> a) in
+      let pos = 5 in
+      let actual = Edit_command.new_list l pos [ -2 ; -1 ] in
+      let conformity li =
+        ((List.nth_exn li pos) = -2) &&
+        ((List.nth_exn li (succ pos)) = -1)
+      in
+      OUnit.assert_bool "My test" (conformity actual)
+    )
+  )
+;;
+
+let lt_l =
+  List.map nl_data ~f:(fun (t, s, name) -> ( (n_list t s), name))
+  |> List.map ~f:(fun ( f,name ) -> (name, `Quick, f))
+;;
+(* =========================================== *)
+
+(* To be used in test.ml *)
+let alco = [( "Edit_command.ml: Epur", llt_l ) ;
+  ( "Edit_command.ml: New list", ( big :: lt_l ) )
+];;
+
+
+
