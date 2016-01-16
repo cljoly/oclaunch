6fe29b99a67142d94649994c335015fa8aa31496
Leo <leowzukw@vmail.me>
2016-01-16 15:57:22 +0100

    Correct syntax errors in pkg.sh script
    Various problem with brackets, creating syntax error for the first and
    producing the archive with a wrong name.

--- pkg.sh
+++ pkg.sh
@@ -4,7 +4,7 @@
 echo "Start"
 
 # If directory doesn't exist, create it
-if ! [[ -e dist ]]; then
+if ! [ -e dist ]; then
     mkdir dist
 fi
 
@@ -14,5 +14,5 @@ id=`git rev-parse --short --verify HEAD`
 name=OcLaunch_${tag}-${id}.tgz
 
 echo "Write in" $name
-git archive master --prefix=${name}/ --format=tgz -o dist/$name
+git archive master --prefix=${name}/ --format=tgz -o dist/${name}
 
