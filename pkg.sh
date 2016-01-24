583f0d23f7b6dbea43ffefc0f261dabc6c4bab51
Leo <leowzukw@vmail.me>
2016-01-24 11:20:49 +0100

    Improve pkg.sh script performance
    .tar.gz files are now taken the tar archive, to avoid the recreation git archive
    was doing.

--- pkg.sh
+++ pkg.sh
@@ -13,12 +13,12 @@ id=`git describe --abbrev=40 --candidates=50 HEAD`
 name=OcLaunch_${id}_src # _src emphasis the difference with binary tarballs
 
 echo "Writing in" $name".*"
-git archive HEAD --prefix=${name}/ --format=tar.gz -o dist/${name}.tar.gz -9
 git archive HEAD --prefix=${name}/ --format=zip -o dist/${name}.zip -9
-# Creating .xz and .bz2 from tar archive
+# Creating .xz .gz and .bz2 from tar archive
 tar_name=${name}.tar
 git archive HEAD --prefix=${name}/ --format=tar -o dist/${tar_name}
 cd dist
+gzip -c9 < ${tar_name} >  ${tar_name}.gz
 bzip2 -c9 < ${tar_name} >  ${tar_name}.bz2
 xz -c9 < ${tar_name} >  ${tar_name}.xz
 
