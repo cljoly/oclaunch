6ece1db8b1f02b8d8e8171424bddb540c6fd9e40
Leo <leowzukw@vmail.me>
2016-02-17 13:15:54 +0100

    Fix behavior of test script, using env variable
     + Uses ability to set parameters via environment variable.
     + Fix error occurring with ./test.sh.

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Some script to test the behavior of the program with custom rc file
-# FIXME Better behavior when launched without arguments
-OC_TMP=/tmp/v033 ./oclaunch.native $* -v 5 -c ./dev.json
+OC_TMP=/tmp/v033 OC_RC="./dev.json" OC_VERB=5 ./oclaunch.native $*
