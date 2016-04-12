2e5ebb34b7b990d794c6e9d4f5c6daa7e27ca858
Leo <leowzukw@vmail.me>
2016-04-12 11:05:06 +0200

    Using script for Gitlab CI
     + The tests were quite successful, we are using it for the CI rules.
     + Complete the script.

--- gitlab-ci.sh
+++ gitlab-ci.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
+# Give OCaml version as first argument
+
 # Inspired by https://github.com/ocaml/ocaml-ci-scripts
 
+# Use -y with evry opam command
+OPAMYES=true
 # Installing opam
-opam init --comp="4.02.2"
-
+opam init --comp="$1"
 eval `opam config env`
 
+# Versions
 opam --version
 opam --git-version
 ocaml -version
 
 # ocamlfind is mandatory to build
-opam install -y ocamlfind
+opam install ocamlfind
 
+# Installing dependancies and testing installation
+opam pin add oclaunch-ci .
 # Building OcLaunch and running tests
 ./configure --enable-tests
-make
-./test.native
+make test
