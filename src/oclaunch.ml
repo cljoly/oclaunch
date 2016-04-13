570102ad82be22f33c7d19230f13fad5a6e71f44
Leo <leowzukw@vmail.me>
2016-04-13 18:24:52 +0200

    Back to dev

--- src/oclaunch.ml
+++ src/oclaunch.ml
@@ -38,7 +38,7 @@ open Core.Std;;
 
 (* Variable to store version number *)
 (* TODO Get value from file *)
-let version_number = "0.3.0-pre2";;
+let version_number = "0.3.0";;
 
 (* Variable store building information *)
 (* XXX This is fake value, it corresponds to the running
